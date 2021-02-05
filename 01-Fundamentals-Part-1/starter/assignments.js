e925a9cc2f11d5a6900018f29a7d7c65ca0e216d
mastersamuke <jedi.master.samuke@gmail.com>
2021-02-05 09:24:18 -0600

    assignments

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,17 @@
+// LECTURE: Values and Variables
+let country = 'United States of America';
+let continent = 'North America';
+let population = '328.2 million';
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// LECTURE: Data Types
+let isIsland = false;
+let language;
+
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
