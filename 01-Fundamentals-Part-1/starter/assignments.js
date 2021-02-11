a6b4110457cde7f3831abcc73108b2ffb673da05
mastersamuke <jedi.master.samuke@gmail.com>
2021-02-11 16:07:20 -0600

    section 2-13

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,19 +1,28 @@
-/*
-// LECTURE: Values and Variables
-let country = 'United States of America';
-let continent = 'North America';
-let population = '328.2 million';
+// // LECTURE: Values and Variables
+// console.log('LECTURE: Values and Variables');
+// const country = 'United States of America';
+// const continent = 'North America';
+// let population = '328.2 million';
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country);
+// console.log(continent);
+// console.log(population);
 
-// LECTURE: Data Types
-let isIsland = false;
-let language;
+// // LECTURE: Data Types
+// console.log('LECTURE: Data Types');
+// const isIsland = false;
+// let language;
 
-console.log(isIsland);
-console.log(population);
-console.log(country);
-console.log(language);
-*/
+// console.log(isIsland);
+// console.log(population);
+// console.log(country);
+// console.log(language);
+
+// // LECTURE: let, const and var
+// console.log('LECTURE: let, const and var');
+// language = 'English';
+
+// console.log(isIsland);
+// console.log(population);
+// console.log(country);
+// console.log(language);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -25,7 +25,6 @@ let job1 = 'programmer';
 let job2 = 'teacher';
 
 console.log(myFirstJob);
-*/
 
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
@@ -46,6 +45,17 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
+*/
 
-let calcAverage = (a, b, c) => (a + b + c) / 3;
-console.log(calcAverage(2, 3, 5));
+let age = 30;
+age = 31;
+
+const birthYear = 1991;
+// birthYear = 1990;
+// const job;
+
+var job = 'programmer';
+job = 'teacher';
+
+lastName = 'Schmedtmann';
+console.log(lastName);
