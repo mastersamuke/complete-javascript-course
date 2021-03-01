9d4b7f1a4c4b26be4de52d1d8940f323eec0a2db
mastersamuke <jedi.master.samuke@gmail.com>
2021-03-01 12:47:07 -0600

    section 2-14 (Basic Operators)

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,28 +1,40 @@
-// // LECTURE: Values and Variables
-// console.log('LECTURE: Values and Variables');
-// const country = 'United States of America';
-// const continent = 'North America';
-// let population = '328.2 million';
+// LECTURE: Values and Variables
+console.log('LECTURE: Values and Variables');
+const country = 'United States of America';
+const continent = 'North America';
+let popNumber = 328200000;
+let population = '328.2 million';
 
-// console.log(country);
-// console.log(continent);
-// console.log(population);
+console.log(country);
+console.log(continent);
+console.log(population);
 
-// // LECTURE: Data Types
-// console.log('LECTURE: Data Types');
-// const isIsland = false;
-// let language;
+// LECTURE: Data Types
+console.log('LECTURE: Data Types');
+const isIsland = false;
+let language;
 
-// console.log(isIsland);
-// console.log(population);
-// console.log(country);
-// console.log(language);
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
 
-// // LECTURE: let, const and var
-// console.log('LECTURE: let, const and var');
-// language = 'English';
+// LECTURE: let, const and var
+console.log('LECTURE: let, const and var');
+language = 'English';
 
-// console.log(isIsland);
-// console.log(population);
-// console.log(country);
-// console.log(language);
+console.log(isIsland);
+console.log(population);
+console.log(country);
+console.log(language);
+
+// LECTURE: Basic Operators
+console.log('LECTURE: Basic Operators');
+const halfPop = popNumber / 2;
+console.log(halfPop);
+popNumber++;
+console.log(popNumber);
+const finlandPop = 6000000;
+const isFinPopSmaller = popNumber > finlandPop;
+const description = country + ' is in ' + continent + ', and its ' + population + ' people speak ' + language;
+console.log(description);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -45,7 +45,6 @@ year = 1991;
 console.log(typeof year);
 
 console.log(typeof null);
-*/
 
 let age = 30;
 age = 31;
@@ -59,3 +58,34 @@ job = 'teacher';
 
 lastName = 'Schmedtmann';
 console.log(lastName);
+*/
+
+// Math operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+// assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--;
+x--;
+console.log(x);
+
+// comparison operators
+console.log(ageJonas > ageSarah);
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
